7ef9ba94e7dc7296c524945c0ec69ae023f360cd
anju khod <khodanju@gmail.com>
2024-01-09 21:43:48 +0530

    water

--- hungry.py
+++ hungry.py
@@ -4,4 +4,7 @@ if hungry == "yes":
     print("eat pizza")
     print("eat burger")
 else:
+    thrusty =input ("are you thirsty?")
+    if thrusty == "yes":
+        print("drink water")
     print("do your homework")
